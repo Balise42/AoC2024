6ac271418b515a96b78de3fe245c6cb55d51437b
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2024-12-23 21:07:59 +0100

    DAY 21 B!!!!!

--- day21.dart
+++ day21.dart
@@ -1,3 +1,5 @@
+import "package:collection/collection.dart";
+
 import "utils.dart";
 
 Map<String, (int, int)> numpad = {
@@ -24,115 +26,101 @@ Map<String, (int, int)> dirpad = {
 
 void main() {
   var lines = fileToStrings('./data/day21.dat');
-  //lines = ['894A'];
-  /*Map<(String, String), int> typeDigit = {};
-  for (var i = 0; i <= 9; i++) {
-    var istr = i.toString();
-    for (var j = 0; j <= 9; j++) {
-      var jstr = j.toString();
-      typeDigit[(istr, jstr)] = typeNum(istr, jstr);
+  var res1 = 0, res2 = 0;
+  for (var line in lines) {
+    var pathLength = typeNum(line, 2);
+    res1 += pathLength * int.parse(line.substring(0, 3));
+    pathLength = typeNum(line, 25);
+    res2 += pathLength * int.parse(line.substring(0, 3));
+  }
+  print(res1);
+  print(res2);
+}
+
+int typeNum(String s, int iter) {
+  String firstRobot = typeDigits(s, 'A');
+
+  var robot = firstRobot;
+  var split = robot.split('A');
+  split.removeLast();
+  robot = '';
+  Map<String, int> robotRep = {};
+  for (var s in split) {
+    robotRep.putIfAbsent(s + 'A', () => 0);
+    robotRep[s + 'A'] = robotRep[s+'A']! + 1;
+  }
+
+  for (var i = 0; i<iter; i++) {
+    Map<String, int> newRep = {};
+    for (var entry in robotRep.entries) {
+      var next = typeRobot(entry.key, 'A');
+      var nextSplit = next.split('A');
+      nextSplit.removeLast();
+      for (var ns in nextSplit) {
+        newRep.putIfAbsent(ns + 'A', () => 0);
+        newRep[ns + 'A'] = newRep[ns + 'A']! + entry.value;
+      }
     }
-    typeDigit[(istr, 'A')] = typeNum(istr, 'A');
-    typeDigit[('A', istr)] = typeNum('A', istr);
-  }*/
-  //typeDigit['A'] = typeNum('A');
+    robotRep = newRep;
+  }
 
   var res = 0;
-  for (var line in lines) {
-    var pathLength = typeNum(line);
-    res += pathLength * int.parse(line.substring(0, 3));
+  for (var rep in robotRep.entries) {
+    res += rep.key.length * rep.value;
   }
-  print(res);
+  return res;
 }
 
-int typeNum(String s) {
-  List<String> firstRobots = typeDigits(s, 'A');
+Map<(String, int), String> memo = {};
 
-  var robots = firstRobots;
-  for (var i = 0; i < 2; i++) {
-    List<String> nextRobots = [];
-    for (var robot in robots) {
-      nextRobots.addAll(typeRobot(robot, 'A'));
-    }
-    robots = nextRobots;
+String splitAndType(String robot, int iter) {
+  if (memo.containsKey((robot, iter))) {
+    return memo[(robot, iter)]!;
   }
 
-  var len = 1000;
-  for (String robot in robots) {
-    if (robot.length < len) {
-      len = robot.length;
-    }
+  if (iter == 0) {
+    return robot;
   }
-  return len;
+  var split = robot.split('A');
+  split.removeLast();
+  var res = '';
+  for (var s in split) {
+    var next = typeRobot(s + 'A', 'A');
+    next = splitAndType(next, iter-1);
+    res += next;
+  }
+  if(iter < 20) {
+    memo[(robot, iter)] = res;
+  }
+  return res;
 }
 
-List<String> typeDigits(String s, String prev) {
+String typeDigits(String s, String prev) {
   if(s == '') {
-    return [''];
+    return '';
   } else {
     var goto = s[0];
     var sofar = getNumpadPaths(prev, goto);
     var next = s.substring(1);
-    List<String> res = [];
-    for (var curr in sofar) {
-      res.addAll(typeDigits(next, goto).map((E) => curr + E));
-    }
-    return res;
+    return sofar + typeDigits(next, goto);
   }
 }
 
-List<String> typeRobot(String firstRobot, String prev) {
+
+String typeRobot(String firstRobot, String prev) {
   if(firstRobot == '') {
-    return [''];
+    return '';
   } else {
     var goto = firstRobot[0];
     var sofar = getDirpadPaths(prev, goto);
     var next = firstRobot.substring(1);
-    List<String> res = [];
-    for (var curr in sofar) {
-      res.addAll(typeRobot(next, goto).map((E) => curr + E));
-    }
-    return res;
+    return sofar + typeRobot(next, goto);
   }
 }
 
- /*
-  for (var line in lines) {
-
-  }
-  // 252324 too high
-  // 239800 too low
-
-  print(res);
-}
-
-String typeNum(String line) {
-  var typed = getNumpadPaths('A', line[0]);
-  for (var i = 0; i < line.length-1; i++) {
-    List<String> tmp = [];
-    for (var str in getNumpadPaths(line[i], line[i + 1])) {
-      tmp.addAll(List<String>.from(typed.map((E) => E + str)));
-    }
-    typed = tmp;
-  }
-  print(typed);
-  return '';
-  //List<String> dirs = typed.fold([], (List<String> init, String E) => List<String>.from(init.add(typeDir(E))));
-}
-
-List<String> typeDir(String line) {
-  return [];
-  /*var typed = getDirpadPaths('A', line[0]) + 'A';
-  for (var i = 0; i<line.length-1; i++) {
-    typed += getDirpadPaths(line[i], line[i+1]);
-    typed += 'A';
-  }
-  return typed;*/
-}*/
-
-List<String> getNumpadPaths(String s, String e) {
+String getNumpadPaths(String s, String e) {
   if (s == e) {
-    return ['A'];
+    return 'A';
   }
 
   var sPos = numpad[s]!;
@@ -141,35 +129,33 @@ List<String> getNumpadPaths(String s, String e) {
   if (sPos.$1 == ePos.$1) {
     var str = goDown(sPos, ePos);
     str += goUp(sPos, ePos);
-    return [str + 'A'];
+    return str + 'A';
   }
 
   if (sPos.$2 == ePos.$2) {
     var str = goLeft(sPos, ePos);
     str += goRight(sPos, ePos);
-    return [str + 'A'];
+    return str + 'A';
   }
 
-  List<String> res = [];
+  if(!((s == '0' || s == 'A') && (e == '1' || e == '4' || e == '7'))
+  && (!((s == '4' || s == '1' || s == '7') && (e == '0' || e == 'A')))) {
+    var str = goLeft(sPos, ePos);
+    str += goDown(sPos, ePos);
+    str += goUp(sPos, ePos);
+    str += goRight(sPos, ePos);
+    return(str + 'A');
+  }
   var str = goRight(sPos, ePos);
   str += goUp(sPos, ePos);
   str += goLeft(sPos, ePos);
   str += goDown(sPos, ePos);
-  res.add(str + 'A');
-  if(!((s == '0' || s == 'A') && (e == '1' || e == '4' || e == '7'))
-  && (!((s == '4' || s == '1' || s == '7') && (e == '0' || e == 'A')))) {
-    str = goDown(sPos, ePos);
-    str += goLeft(sPos, ePos);
-    str += goUp(sPos, ePos);
-    str += goRight(sPos, ePos);
-    res.add(str + 'A');
-  }
-  return res;
+  return str + 'A';
 }
 
-List<String> getDirpadPaths(String s, String e) {
+String getDirpadPaths(String s, String e) {
   if(s == e) {
-    return ['A'];
+    return 'A';
   }
 
   var sPos = dirpad[s]!;
@@ -178,31 +164,30 @@ List<String> getDirpadPaths(String s, String e) {
   if (sPos.$1 == ePos.$1) {
     var str = goDown(sPos, ePos);
     str += goUp(sPos, ePos);
-    return [str + 'A'];
+    str + 'A';
   }
 
   if (sPos.$2 == ePos.$2) {
     var str = goLeft(sPos, ePos);
     str += goRight(sPos, ePos);
-    return [str + 'A'];
+    return str + 'A';
   }
 
-  List<String> res = [];
+  if(!(e == '<' && (s == '^' || s == 'A')) &&
+      !(s == '<' && (e == '^' || e == 'A'))) {
+    var str = goLeft(sPos, ePos);
+    str += goDown(sPos, ePos);
+    str += goUp(sPos, ePos);
+    str += goRight(sPos, ePos);
+    return(str + 'A');
+  }
+
   var str = goRight(sPos, ePos);
   str += goDown(sPos, ePos);
   str += goLeft(sPos, ePos);
   str += goUp(sPos, ePos);
-  res.add(str + 'A');
-  if(!(e == '<' && (s == '^' || s == 'A')) &&
-      !(s == '<' && (e == '^' || e == 'A'))) {
-    str = goUp(sPos, ePos);
-    str += goLeft(sPos, ePos);
-    str += goDown(sPos, ePos);
-    str += goRight(sPos, ePos);
-    res.add(str + 'A');
-  }
+  return(str + 'A');
 
-  return res;
 }
 
 String goRight((int, int) sPos, (int, int) ePos) {
